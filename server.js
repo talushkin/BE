8ed27c2db8a628fa02ad6cd5be7cf19fb2b239f3
talushkin <33185889+talushkin@users.noreply.github.com>
2025-05-14 20:16:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config();
 
 const categoryRoutes = require("./routes/categoriesRoutes");
 const recipeRoutes = require("./routes/recipesRoutes");
