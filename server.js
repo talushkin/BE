d37a9765affe24b3ad81f4948ae597183274e442
talushkin <33185889+talushkin@users.noreply.github.com>
2025-05-16 14:51:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,18 +10,17 @@ const logger = require("./middlewares/logger"); // 👈 מוסיפים את ה-l
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const connectionString = process.env.MONGODB_CONNECTION_STRING || "mongodb://localhost:27017/recipes";
 app.use(cors());
 app.use(express.json());
 app.use(logger); // 👈 מוסיפים אותו לפני הרואטרים
 
-mongoose.connect("mongodb+srv://ipadtal:puKakhPNgavqHAvZ@talushkin.s3mepas.mongodb.net/recipes?retryWrites=true&w=majority&appName=talushkin", {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.status(200).send("BE Server Recipes Tal Arnon node is Running on port :"+PORT+" / "+process.env.TOKEN));
 app.use("/api/recipes", recipeRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/ai", openAIRoutes);
