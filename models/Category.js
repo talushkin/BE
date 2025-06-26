c54c0ed8c84a7997f9465575d50f1986babb816b
Tal <98785995+smiling-house@users.noreply.github.com>
2025-06-26 11:15:42 +0300

    add translations of category and recipe modules

--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,11 @@ const categorySchema = new mongoose.Schema({
     translated: { type: Boolean, default: false },
     language: { type: String, default: "he" },
     createdAt: { type: Date, default: Date.now },
-    description: String
+    description: String,
+    translatedCategory: [{
+        lang: { type: String, required: true },
+        value: { type: String, required: true }
+    }]
 });
 
 module.exports = mongoose.model("Category", categorySchema);
--- models/Recipe.js
+++ models/Recipe.js
@@ -9,7 +9,10 @@ const recipeSchema = new mongoose.Schema({
     imageUrl: String,
     translated: { type: Boolean, default: false },
     language: { type: String, default: "en" },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    translatedTitle: [{ lang: { type: String, required: true }, value: { type: String, required: true } }],
+    translatedIngredients: [{ lang: { type: String, required: true }, value: [{ type: String, required: true }] }],
+    translatedPreparation: [{ lang: { type: String, required: true }, value: { type: String, required: true } }]
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
